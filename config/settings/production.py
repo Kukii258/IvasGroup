f655d6a194b89edc6fc5b6a766d1b34ef07c0fc9
kukelj <kukelj.luka@gmail.com>
2025-03-04 11:57:20 +0100

    Fix ALLOWED_HOSTS issue

--- config/settings/production.py
+++ config/settings/production.py
@@ -10,7 +10,9 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+import os
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "95.111.252.129").split(",")
 
 # DATABASES
 # ------------------------------------------------------------------------------
